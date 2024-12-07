2d70445d13e31f52a2ae5577973ab9365c1923e8
Anik Dakua <anikdakua@gmail.com>
2024-12-07 11:45:55 +0530

    Before updating to angular 19 and angular material 19

--- src/app/components/material/material.component.ts
+++ src/app/components/material/material.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-import { MatIconModule } from '@angular/material/icon';
+import { Clerk } from "@clerk/clerk-js";
+import { SignInProps } from '@clerk/clerk-js/dist/types/ui/types';
+import { Theme } from '@clerk/types';
 
 @Component({
   selector: 'app-material',
@@ -21,8 +24,38 @@ export class MaterialComponent {
 
   myForm !: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  key = "pk_test_cHJpbWFyeS13aWxkY2F0LTM1LmNsZXJrLmFjY291bnRzLmRldiQ";
+
+  clerk = new Clerk(this.key);
+
+  signInTheme: Theme = {
+    // baseTheme: ,
+    layout: {
+      logoImageUrl: "",
+      logoLinkUrl: "",
+      logoPlacement: "inside",
+      socialButtonsPlacement: "bottom",
+      showOptionalFields: true,
+      socialButtonsVariant: "iconButton"
+    },
+    variables: {
+      colorBackground: "white",
+      fontSize: "1.2rem",
+      fontWeight: {
+        normal: 100,
+        medium: 300,
+        // semibold : 500,
+        bold: 600
+      },
+      spacingUnit: "0.5rem"
+    }
+  }
+
+  constructor(private fb: FormBuilder, private elRef: ElementRef) {
     this.initializeForm();
+
+    this.clerk.load();
+
   }
 
   initializeForm() {
@@ -35,5 +68,36 @@ export class MaterialComponent {
 
   submitForm() {
     console.log("my form value : ", this.myForm);
+
+    const signinProp: SignInProps = {
+      forceRedirectUrl: "string | null",
+      fallbackRedirectUrl: "",
+      signUpUrl: "string",
+      appearance: this.signInTheme,
+      initialValues: {
+        emailAddress: "",
+        phoneNumber: "",
+        username: "",
+      },
+      waitlistUrl: "string",
+      __experimental: {
+        newComponents: true
+      }
+    };
+
+    // 3 basics
+    // this.clerk.openSignIn(signinProp);
+
+    // this.clerk.openSignUp();
+    // this.clerk.openUserProfile();
+
+    // Mount the SignIn component on the div with a specific id
+    // const signInDiv = this.elRef.nativeElement.querySelector('#clerk-signin');
+    // this.clerk.mountSignIn(signInDiv);
+
+    this.clerk.openGoogleOneTap();
+
+    // this.clerk.openCreateOrganization(); // feature is turned off 
+
   }
 }
